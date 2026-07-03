73e4c4139ae75614d203972d9aa7e75288bbfb76
ucihafafa-alt <ucihafafa@gmail.com>
2026-07-03 11:22:01 +0800

    Add files via upload

---
+++ README-v40.txt
@@ -0,0 +1,1 @@
+Mungu v40 final: тайлангийн engine шинэчлэгдсэн, 9,900₮ багц 3 нүд нээнэ, iPhone банкны app open/copy fix орсон. GitHub root дээр бүх файлыг Replace хийгээд force-reset.html нээнэ.
--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_VERSION='tm-v38-final-no-cache';
+const CACHE_VERSION='tm-v40-report-ios-no-cache';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(() => caches.match(event.request)));
 });
