d99df3c32d0dcb3bb0406df0edde5b7e41b9b8cb
ucihafafa-alt <ucihafafa@gmail.com>
2026-07-03 19:02:46 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='tm-v43-fate-number-no-cache';
+const CACHE_VERSION='tm-v44-fate-number-no-cache';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
