058d799c3aae6cd9d6d4c661074aafe612b69e7c
ucihafafa-alt <ucihafafa@gmail.com>
2026-07-23 00:09:31 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_VERSION='tm-v44-fate-number-no-cache';
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request,{cache:'no-store'}).catch(() => caches.match(event.request)));
-});
